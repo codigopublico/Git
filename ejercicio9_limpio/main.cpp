f93e55fc44a2c51cd31a5a155c2de092e1a7ee7c
iper <iper@prog.com>
2017-10-02 17:01:38 +0200

    hola

---
+++ ejercicio9_limpio/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: iper
+ *
+ * Created on 1 de octubre de 2017, 15:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * Este es para hacer la limpieza de ejercicio9 De modo que se hara de forma progresiva.
+ */
+class equipo{
+    //eta classe es la del equipo con todos sus metodos i funciones que se heradara de todas las demas.
+};
+int setnombres(equipo *constante){
+    //eta es la funcion que se encarga de escanear  todo lo del teclado.
+    //tengo que hacer que se haga todo por teclado.
+}
+class partidos : public equipo  {
+    // Esta se carga de jugar los partidos.
+};
+class liga : public partidos   {
+    //ete se encarga de organizar de esquematizar los partidos en base a una liga
+};
+void fliga(){
+//Esta es la funcion madre de todo el proyecto que organizara todas las classes de arriva en objetos.
+}
+int main(int argc, char** argv) {
+    fliga();
+    return 0;
+}
+
