1d963c8d88aa7c2f9df9ba7485237d4ac09b3bf4
iper <iper@prog.com>
2017-10-13 17:31:54 +0200

    hola

--- capitulo10/main.cpp
+++ capitulo10/main.cpp
@@ -11,39 +11,79 @@
 using namespace std;
 
 /*
- * 
+ *Falta ponerle las classes i  classificar para ello i hacerlo random. Todo. Para manya
  */
+int max = 200;
+
+
 class alumno{
 public:
     int max;
-    string *nombre;
+    string nombre;
     int *notas;
     int conta;
-    int ref[200];
     alumno(){
         max = 200;
-        string *nom = new string[max];
-        nombre = nom;
-        int *notas = new int[max];
+        int *nota = new int[max];
+        notas = nota;
         conta = 0;
     }
-    void poner(string inom){
-        nombre[conta] = inom;
-        ref[conta] = conta;
-        conta++;
+    void igresar(string inom){
+        nombre = inom;
     }
     void mostrar(){
-        for(int i = 0; i <= conta; i++){
-            std::cout << nombre[ref[i]] << "\n";
+            std::cout << nombre << "\n";
+    }
+    void pnotas(int inota){
+        notas[conta] = inota; 
+        std::cout << "Poniendo la nota " << inota << " a " << nombre;
+        conta++;
+    }
+};
+class classe{
+public:
+    alumno nom[200];
+    int conta;
+    string nclasse;
+    classe(string inclasse){
+        conta = 0;
+        nclasse = inclasse;
+    }
+    void entar(alumno inom, int total){
+        int i = 0;
+        for(; i < total; i++ ){
+            nom[conta + i] = inom;
+        }
+        conta = i + conta;
+    }
+    void saludo(){
+        for(int i = 0; i < conta; i++){
+        std::cout << "Te saluda " << nom[i].nombre << "\n";
+        }
+    }
+    void mnotas(){
+    for(int i = 0; i < conta; i++){
+        std::cout << "\nNotas de  " << nom[i].nombre << "  ";
+        for(int ii = 0; ii < nom[i].conta; ii++){
+            std::cout << "  " << nom[i].notas[ii] << "\n";
+        }
+        std::cout << "\n";
         }
     }
 };
-
 void final(){
     alumno virtua;
-    virtua.poner("maria");
-    virtua.poner("Joan");
-    virtua.mostrar();
+    alumno alex;
+    classe mates("mates");
+    string nom2[] = {"maria", "Joan"};
+    alex.igresar(nom2[1]);
+    alex.pnotas(8);
+    virtua.igresar(nom2[0]);
+    mates.entar(virtua, 1);
+    mates.entar(alex, 1);
+    mates.saludo();
+    mates.mnotas();
+    
 }
 int main(int argc, char** argv) {
     final();
