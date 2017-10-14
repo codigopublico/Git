0bc2f3ef956cbafe1afbeee2c6b646199227e945
iper <iper@prog.com>
2017-10-14 13:32:20 +0200

    capitulo10س

--- capitulo10/main.cpp
+++ capitulo10/main.cpp
@@ -38,7 +38,7 @@ public:
     }
     void pnotas(int inota){
         notas[conta] = inota; 
-        std::cout << "Poniendo la nota " << inota << " a " << nombre;
+        //std::cout << "Poniendo la nota " << inota << " a " << nombre;
         conta++;
     }
 };
@@ -50,6 +50,10 @@ public:
     classe(string inclasse){
         conta = 0;
         nclasse = inclasse;
+    }
+    classe(){
+        conta = 0;
+        nclasse = "Sin_classe";
     }
     void entar(alumno inom, int total){
         int i = 0;
@@ -67,7 +71,7 @@ public:
     for(int i = 0; i < conta; i++){
         std::cout << "\nNotas de  " << nom[i].nombre << "  ";
         for(int ii = 0; ii < nom[i].conta; ii++){
-            std::cout << "  " << nom[i].notas[ii] << " de la classe  " << nom[i].classes[ii] ;
+            std::cout << "  " << nom[i].notas[ii] << " de la classe  " << nom[i].classes[ii]  << "\n";
         }
         std::cout << "\n";
         }
@@ -76,17 +80,85 @@ public:
         nom[ref].classes[nom[ref].conta] = nclasse;
         nom[ref].pnotas(nota);
     }
+    void cmedia(){
+        int sum = 0;
+        int sum2 = 0;
+        for(int i = 0; i < conta; i++){
+            for(int ii = 0; ii < nom[i].conta; ii++){
+            sum = nom[i].notas[ii] + sum;
+            sum2++;
+            }
+        }
+        std::cout << "\nLa nota media de la classe   " << nclasse << "  es:"  << sum/sum2 << "\n";
+    }
+    void amedia(){
+        int no[conta];
+        int re[conta];
+    for(int i = 0; i < conta; i++){
+       std::cout << "\nNotas de  media de " << nom[i].nombre << "  ";
+            for(int ii = 0; ii < nom[i].conta; ii++){
+                no[i] = no[i] + nom[i].notas[ii];
+                re[i]++;
+            }
+       std::cout << " es de " << no[i]/re[i] << " de la classe " << nclasse << "\n";
+    }
+
+    }
+    
+};
+class director{
+public:
+    classe res[200];
+    int conta;
+    director(){
+        conta = 0;
+    }
+    void ingresar_classe(classe virtua){
+        std::cout << "Haciendo la classe virtual\n";
+                res[conta] = virtua;
+                conta++;
+    }
+    void mostrar(){
+        std::cout << "hola\n"; 
+        for(int i = 0; i < conta; i++){
+            for(int ii = 0; ii < res[i].nom[i].conta; ii++){
+        std::cout << "Los alumnos son " << res[i].nom[ii].nombre << " de la classe "<<res[i].nom[ii].classes ;
+        }
+        }
+    } 
 };
 void final(){
     int aux = 20;
     string nom = "alex";
+    string fem = "laura";
     alumno hola;
+    alumno laura;
+    laura.igresar(fem);
     classe mates("mates");
+    classe sofware("Sofware");
     hola.igresar(nom);
     for(int i = 0; i < aux; i++){
-        classe.entar(hola, 1);
+        mates.entar(hola, 1);
+        sofware.entar(laura, 1);
     }
-    classe.saludo();
+    mates.saludo();
+    sofware.saludo();
+    for(int i = 0; i < aux; i++){
+        for(int ii = 0; ii < aux; ii++){
+        mates.pnotas(rand()% 10, i);
+        sofware.pnotas(rand()% 10, i);
+        }
+    }
+    //mates.mnotas();
+    sofware.mnotas();
+    sofware.cmedia();
+    mates.cmedia();
+    //mates.amedia();
+    //sofware.amedia();
+    director bigdata;
+    //bigdata.ingresar_classe(mates);
+    //bigdata.ingresar_classe(sofware);
+    //bigdata.mostrar();
     
 }
 int main(int argc, char** argv) {
