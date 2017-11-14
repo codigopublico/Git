69e83cb2d11c1730f89184c94c70ea35e2c4e223
iper <iper@prog.com>
2017-11-14 19:23:24 +0100

    hola

---
+++ capitulo16/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: iper
+ *
+ * Created on 14 de noviembre de 2017, 19:13
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * Este es del ejercio 16
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
