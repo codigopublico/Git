244af56efef7dbeacd5b096ad80d17ecfbddf3ea
iper <iper@prog.com>
2017-12-05 19:02:13 +0100

    nueva_rama

--- capitulo16/hospi.cpp
+++ capitulo16/hospi.cpp
@@ -1,1 +1,68 @@
+#include "hospi.hpp"
+using namespace hos;
+void hos::limpia(){
+    for(int i = 0; i < 2; i++){
+        std::cout << "\n";
+    }
+}
+void hos::limpia(const int &a){
+    for(int i = 0; i < a; i++){
+        std::cout << "\n";
+    }
+}
+//esta es la seccion de persona
+persona::persona(){
+    std::cout << "Estas ejecuntado el el constructor de Persona\n";
+};
+persona::persona(const string &a, const int &b){
+    persona::Spers.Nombre = a; 
+    persona::Spers.telf = b;
+    std::cout << "Constructor.....\n";
+    std::cout << "Nombre   :" << persona::Spers.Nombre << "\n";
+    std::cout << "telefono :" << persona::Spers.telf << "\n";
+}
+virtual void persona::ver(string &a, int &b){
+    a = persona::Spers.Nombre;
+    b = persona::Spers.telf;
+}
+//esta es la seccion de profecional
+profecional::profecional(){
+    std::cout << "Estas ejecutando el construcotior de profecional\n";
+};
+profecional::profecional(const string &a, const int &b){
+        persona::Spers.Nombre = a;
+        persona::Spers.telf = b;
+        std::cout << "Constructor.....\n";
+    std::cout << "Nombre   :" << persona::Spers.Nombre << "\n";
+    std::cout << "telefono :" << persona::Spers.telf << "\n";
+};
+profecional::profecional(const string &a, const int &b, const float &c, const int d[2]){
+    persona::Spers.Nombre = a;
+        persona::Spers.Nombre = b;
+        profecional::Sprof.Planta = c;
+        for(int i = 0; i < 2; i++){
+        profecional::Sprof.horario[i] = d[i];
+        }
+
+};
+profecional profecional::operator =(const profecional &a){
+    profecional res;
+    res.Spers.Nombre = a.Spers.Nombre;
+    res.Spers.telf = a.Spers.telf;
+    res.Sprof.Planta = a.Sprof.Planta;
+    res.Sprof.especialidad = a.Sprof.especialidad;
+    for(int i = 0; i < 2; i++){
+    res.Sprof.horario[i] = a.Sprof.horario[i];
+    }
+}
+/*profecional* profecional::ver(){
+    *Hprof->Spers.Nombre = profecional::Spers.Nombre;
+    *Hprof->Spers.telf = profecional::Spers.telf;
+    *Hprof->Sprof.Planta = profecional::Sprof.Planta;
+    *Hprof->Sprof.especialidad  = profecional::Sprof.especialidad;
+    for(int i = 0; i < 2; i++){
+    *Hprof->Sprof.horario[i] = profecional::Sprof.horario[i];
+    }
+    return *Hprof;
+}*///esto es una cajada que no sirve para nada.....
 
--- capitulo16/hospi.hpp
+++ capitulo16/hospi.hpp
@@ -11,10 +11,80 @@
 #include <string>
 using namespace std;
 namespace hos{
+    void limpia();
+    void limpia(const int&);
+    class persona{
+    public:
+        virtual void ver(string&, int&);
+        persona();
+        persona(const string&, const int&);
+        struct Spersona{
+             string Nombre;
+        int telf;
+        Spersona(){
+            Nombre = "0";
+            telf = 0;
+        };//este es el contructor de la classe
+        Spersona operator=(const Spersona &a){//tengo que hacer un operador igual que este pero para eliminar en la alura de profecional.
+            Spersona res;
+            res.Nombre = a.Nombre;
+            res.telf = a.telf;
+            return res;
+        }//este es el contructor de copias para el editor.
+        void operator-(Spersona &a){
+            a.Nombre = "0";
+            a.telf = 0;
+        }
+}Spers;
+
+        };
+ class profecional : public persona{
+ public: 
+     //profecional *Hprof;
+     profecional();
+     profecional(const string&, const int&);
+     profecional(const string&, const int&, const float&, const int[2]);
+     profecional operator=(const profecional&);
+     //profecional* ver();
+     //variables......
+     int n; 
+     struct Sprofecional{
+        float Planta;
+        string especialidad;
+        int horario[2]; //una es la que emplieza i la otra la que acaba;
+        Sprofecional(){
+            Planta = 0;
+            especialidad = "0";
+            for(int i = 0; i < 2; i++){
+                horario[i] = 0;
+                
+            }
+        }
+        Sprofecional operator=(const Sprofecional &a){
+            Sprofecional res;
+            res.Planta = a.Planta;
+            res.especialidad = a.especialidad;
+            for(int i = 0; i < 2; i++){
+            res.horario[i] = res.horario[i];
+            }
+        }
+        void operator-(Sprofecional &a){
+            a.Planta = 0;
+            a.especialidad = "0";
+            for(int i = 0; i < 2; i++){
+            a.horario[i] = 0;
+            }
+        }
+}Sprof;
+
+     
+ };
+       
+    }
+
     
     
-    
-}
+
 
 
 #endif	/* HOSPI_HPP */
