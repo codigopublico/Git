9479d195c449cd69dbc165d8ce342e8c060faccd
iper <iper@prog.com>
2017-12-10 22:12:37 +0100

    nueva_rama

---
+++ capitulo18/capitulo18/biblio.hpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   biblio.hpp
+ * Author: iper
+ *
+ * Created on 9 de diciembre de 2017, 20:56
+ */
+
+#ifndef BIBLIO_HPP
+#define	BIBLIO_HPP
+class material{
+public:
+    
+};
+
+
+#endif	/* BIBLIO_HPP */
+
---
+++ capitulo18/capitulo18/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: iper
+ *
+ * Created on 9 de diciembre de 2017, 20:53
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
